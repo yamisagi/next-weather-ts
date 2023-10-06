4bb725bff3f93b47560d8a6d4bf57c59e842642e
Yamisagi <erencandan20@gmail.com>
2023-10-06 23:08:53 +0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
+
+export const metadata: Metadata = {
+  title: 'Weather App',
+  description: 'Weather App',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
